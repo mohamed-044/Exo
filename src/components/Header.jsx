a61c93373b9d6ef6ac7fe888a2fe3c05eddbb2fb
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-20 10:07:17 +0200

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
+import logo from '../assets/logo.webp';
 function Header() {
   return (
     <header>
-      <img
-        src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-        alt="Un mexicain avec un chapeau qui joue de la guitare"
-      />
+      <img src={logo} alt="Un mexicain avec un chapeau qui joue de la guitare" />
       <nav>
         <ul>
           <li>

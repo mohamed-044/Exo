60492606e016c1684c3a70ec08f3c568debd0b2f
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-20 10:25:59 +0200

    add new badge

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,19 @@ function App() {
           title="Tacos à l’unité"
           image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
           price={3}
+          isNew={true}
         />
         <Dish
           title="Enchiladas"
           image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
           price={12}
+          isNew={false}
         />
         <Dish
           title="Mole poblano"
           image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
           price={15}
+          isNew={false}
         />
       </main>
       <Footer />
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,9 +1,14 @@
-function Dish({ image, title, price }) {
+import Badge from 'react-bootstrap/Badge';
+import Stack from 'react-bootstrap/Stack';
+function Dish({ image, title, price, isNew }) {
   return (
     <div>
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>{price}€</p>
+      {isNew == true && <Stack direction="horizontal" gap={2}>
+      <Badge bg="primary">New</Badge>
+      </Stack>}
     </div>
   );
 }

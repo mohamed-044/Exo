e3b635947b4816d17d827511a6a357b6a306424d
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-05-21 12:02:01 +0200

    filter dishes in stock

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,21 @@ function App() {
       image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       price: 3,
       isNew: true,
-      inStock : true,
+      inStock : 12,
     },
     {
       title: "Enchiladas",
       image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       price: 12,
       isNew: false,
-      inStock : true,
+      inStock : 5,
     },
     {
       title: "Mole poblano",
       image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       price: 15,
       isNew: false,
-      inStock : false,
+      inStock : 0,
     },
   ];
 
